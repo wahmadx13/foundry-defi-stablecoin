0548b45cf7d624a1f6c2b7d93f36e47a84904c06
Waseem <wasim_ahmad2003@yahoo.com>
2024-09-24 15:46:06 +0500

    added basic configuration and contract

---
+++ src/DecentralizedStableCoin.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+/**
+* @title DecentralizedStableCoin
+* @author Waseem Ahmad
+* Collateral: Exogonous (ETH & BTC)
+* Minting: ALgorithmic
+* Relative Stability: Pegged to USD
+
+* This is the contract menat to be governed by DSCEngine. This contract is just ERC20 implenetation of our stablecoin system
+ */
+
+contract DecentralizedStableCoin {}
