da8bc99ea4918f5c24927ab2b6312e2316ad1d25
Waseem <wasim_ahmad2003@yahoo.com>
2024-09-24 16:46:15 +0500

    setup constructor of DSCEngine.sol

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
+import {DecentralizedStableCoin} from "src/DecentralizedStableCoin.sol";
+
 /**
  * @title DSCEngine
  * @author Waseem Ahmad
@@ -18,6 +20,49 @@ pragma solidity ^0.8.18;
  * @notice This contract is VERY loosely based on the MakerDAO DSS (DAI) system.
  */
 contract DSCEngine {
+    /////////////////////
+    // Errors          //
+    /////////////////////
+    error DSCEngine__NeedsMoreThanZero();
+    error DSCEngine__TokenAddressesAndPriceFeedAddressesMustBeSameLength();
+
+    /////////////////////
+    // State Variables //
+    /////////////////////
+    mapping(address token => address priceFeed) private s_priceFeeds; // tokenToPriceFeed
+
+    DecentralizedStableCoin private immutable i_dsc;
+
+    /////////////////////
+    // Modifiers       //
+    /////////////////////
+
+    // modifier isAllowedToken(address token) {
+
+    // }
+
+    /////////////////////
+    // Functions       //
+    /////////////////////
+    constructor(
+        address[] memory tokenAddresses,
+        address[] memory priceFeedAddresses,
+        address dscAddress
+    ) {
+        // USD Price Feeds
+        if (tokenAddresses.length != priceFeedAddresses.length) {
+            revert DSCEngine__TokenAddressesAndPriceFeedAddressesMustBeSameLength();
+        }
+        // For example ETH / USD, BTC, MKR / USD, etc.,
+        for (uint256 i = 0; i < tokenAddresses.length; i++) {
+            s_priceFeeds[tokenAddresses[i]] = priceFeedAddresses[i];
+        }
+        i_dsc = DecentralizedStableCoin(dscAddress);
+    }
+
+    ////////////////////////
+    // External Functions //
+    ////////////////////////
     function depositCollateralAndMintDSC() external {}
 
     function depositCollateral() external {}
