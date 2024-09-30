eab1a925705b891b94320ff125a99eefb33ddf4f
Waseem <wasim_ahmad2003@yahoo.com>
2024-09-30 15:27:07 +0500

    setup basic DSCEngineTest file

---
+++ test/unit/DSCEngineTest.t.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {Test} from "forge-std/Test.sol";
+import {DSCEngine} from "src/DSCEngine.sol";
+import {DeployDSC} from "script/DeployDSC.s.sol";
+import {DecentralizedStableCoin} from "src/DecentralizedStableCoin.sol";
+
+contract DSCEngineTest is Test {
+    DeployDSC deployer;
+    DecentralizedStableCoin dsc;
+    DSCEngine dscEngine;
+
+    function setUp() public {
+        deployer = new DeployDSC();
+        (dsc, dscEngine) = deployer.run();
+    }
+}
